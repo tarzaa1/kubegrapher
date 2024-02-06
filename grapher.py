6261b12b82fa9aabb71a31b2b9c869a56d3077be
Tarek Zaarour <tareqzaarour@gmail.com>
2024-02-06 10:32:14 +0000

    add interfaces

--- grapher.py
+++ grapher.py
@@ -1,7 +1,6 @@
 from kubegrapher.utils.graph.neo4j import Neo4j
-from kubegrapher.utils.utils import merge_node, merge_relationship
-from .model import Node
-from pprint import pprint
+from kubegrapher.model import Image, Container, Label, Taint, Annotation, Pod, Deployment, ReplicaSet, K8sNode, ConfigMap, Service
+from kubegrapher.utils.utils import delete_node as delete
 
 
 class Grapher(object):
@@ -11,95 +10,48 @@ class Grapher(object):
     def add_cluster(self) -> None:
         pass
 
-    def add_k8snode(self, tx, properties: dict[str: any] = {}, labels: dict[str: str] = {}, annotations: dict[str: str] = {}, taints: list[dict[str: str]] = [], images: list[dict[str: any]] = []) -> None:
-        merge_node_query = merge_node(type='K8sNode', properties=properties)
-        result = tx.run(merge_node_query, properties)
-        node = result.single().data()['node']
-        print(node)
-        for label in labels.items():
-            result = self.add_label(tx, *label)
-            l = result.single().data()['node']
-            print(l)
-            connect_label = merge_relationship(type='HAS_LABEL', from_type='K8sNode', from_properties={
-                                               'name': node['name']}, to_type='Label', to_properties=l)
-            print(connect_label)
-            connect_label = f"""MATCH (node:K8sNode {{name: $name}}), (label:Label {{key: $key, value: $value}})
-                                MERGE p = (node)-[relation:HAS_LABEL]->(label)
-                                RETURN p
-                            """
-            result = tx.run(
-                connect_label, name=node['name'], key=l['key'], value=l['value'])
-            print(result.single().data())
+    def add_k8snode(self, tx, k8snode: K8sNode):
+        k8snode.merge(tx)
+        # link node to cluster
 
-        for annotation in annotations.items():
-            result = self.add_annotation(tx, *annotation)
-            annot = result.single().data()['node']
-            print(annot)
-            connect_annotation = f"""MATCH (node:K8sNode {{name: $name}}), (annot:Annotation {{key: $key, value: $value}})
-                                MERGE p = (node)-[relation:HAS_ANNOTATION]->(annot)
-                                RETURN p
-                                """
-            result = tx.run(
-                connect_annotation, name=node['name'], key=annot['key'], value=annot['value'])
-            print(result.single().data())
+    def add_deployment(self, tx, deployment: Deployment):
+        deployment.merge(tx)
 
-        for taint in taints:
-            pass
-            # result = self.add_taint(tx, **taint)
-            # t = result.single().data()['node']
-            # print(t)
-            # connect_taint = f''
-            # result = tx.run()
-            # print(result.single().data())
+    def add_replicaset(self, tx, replicaset: ReplicaSet):
+        replicaset.merge(tx)
 
-        # for image in images:
-        #     merge_image = f"""MERGE (node:Label {{{placeholders(image)}}})
-        #                  RETURN node
-        #                  """
-        #     result = tx.run(merge_image, image)
-        #     connect_image = ''
-        #     result = tx.run(connect_image)
+    def add_pod(self, tx, pod: Pod):
+        pod.merge(tx)
 
-    def add_label(self, tx, key, value) -> None:
-        query = merge_node(type='Label', key=key, value=value)
-        result = tx.run(query, key=key, value=value)
-        return result
+    def add_container(self, tx, container: Container):
+        container.merge(tx)
+    
+    def add_service(self, tx, service: Service):
+        service.merge(tx)
 
-    def add_annotation(self, tx, key, value) -> None:
-        query = merge_node(type='Annotation', key=key, value=value)
-        result = tx.run(query, key=key, value=value)
-        return result
+    def add_configmap(self, tx, configmap: ConfigMap):
+        configmap.merge(tx)
 
-    def add_taint(self, tx, key, effect) -> None:
-        query = merge_node(type='Taint', key=key, effect=effect)
-        result = tx.run(query, key=key, effect=effect)
-        return result
+    def add_label(self, tx, label: Label):
+        label.merge(tx)
 
-    def add_image(self, tx, name, sizeBytes) -> None:
-        query = merge_node(type='Image', name=name, sizeBytes=sizeBytes)
-        result = tx.run(query, name=name, sizeBytes=sizeBytes)
-        return result
+    def add_annotation(self, tx, annotation: Annotation):
+        annotation.merge(tx)
+    
+    def add_taint(self, tx, taint: Taint):
+        taint.merge(tx)
 
-    def add_deployment(self) -> None:
-        pass
+    def add_image(self, tx, image: Image):
+        image.merge(tx)
 
-    def add_replicaset(self) -> None:
-        pass
-
-    def add_pod(self) -> None:
-        pass
-
-    def add_service(self) -> None:
-        pass
-
-    def add_container(self) -> None:
-        pass
-
-    def add_configmap(self) -> None:
-        pass
-
-    def add_secret(self) -> None:
-        pass
+    def delete_pod(self, tx, id=None, **kwargs):
+        self.delete_node(tx, type='Pod,' id=id, **kwargs)
+    
+    def delete_node(self, tx, type, id=None, **kwargs):
+        query = delete(type=type, id=id, **kwargs)
+        print('\n' + query + '\n')
+        result = tx.run(query, id=id, **kwargs)
+        print(result.consume().counters.nodes_deleted)
 
 
 if __name__ == '__main__':
@@ -120,18 +72,36 @@ if __name__ == '__main__':
         'kernel': '5.15.0-87-generic',
         'memory': 45763298
     }
+    uid = 'whatever'
 
-    labels = {
-        'role': 'control plane',
-        'architecture': 'amd64'
-    }
+    label_1 = Label(key='role', value='control_plane')
+    label_2 = Label(key= 'architecture', value='amd64')
 
-    annoations = {
-        'network': 'vxlan'
-    }
+    labels = [label_1, label_2]
+
+    annotation = Annotation(key='network', value='vxlan')
+    annotations = [annotation]
+
+    taint = Taint(key='NoSchedule', effect='node-role.kubernetes.io/control-plane')
+    taints = [taint]
+
+    image = Image('someimageid', 'registry.k8s.io/etcd:3.5.6-0', 102542580)
+    images = [image]
+
+    k8snode = K8sNode(uid, properties, labels, annotations, taints, images)
+    deployment = Deployment('some_uid')
+    replicaset = ReplicaSet('some_other_uid', 'some_uid')
+    configmap = ConfigMap('config_uid', {'name': 'my_configmap'})
+
+    container = Container('someimageid', properties, 'my_configmap')
 
     session = graphdb.driver.session()
-    session.execute_write(grapher.add_k8snode, properties, labels, annoations)
+    session.execute_write(grapher.add_k8snode, k8snode)
+    session.execute_write(grapher.add_deployment, deployment)
+    session.execute_write(grapher.add_replicaset, replicaset)
+    session.execute_write(grapher.add_configmap, configmap)
+    session.execute_write(grapher.add_container, container)
+    # session.execute_write(grapher.delete_node, type='ConfigMap', name='my_configmap')
 
     session.close()
 
