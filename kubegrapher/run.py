c96a664cf23d01780063bc766bdbdc096408b162
Tarek Zaarour <tareqzaarour@gmail.com>
2024-06-10 09:00:33 +0100

    added env vars via conf.py and imported hedera and kafka from source

--- kubegrapher/run.py
+++ kubegrapher/run.py
@@ -1,4 +1,5 @@
 from kubegrapher.utils.graph.neo4j import Neo4j
+from kubegrapher.utils.source import Hedera, Kafka
 from kubegrapher.grapher import Grapher
 from kubegrapher.model import K8sNode
 import kubegrapher.parser as parser
@@ -8,24 +9,25 @@ import csv
 import json
 import pytz
 
-timezone = pytz.timezone("Europe/London")
+from kubegrapher.conf import (
+    DATA_SOURCE,
+    TIMEZONE,
+    KAFKA_BROKER_URL,
+    KAFKA_GROUP_ID,
+    KAFKA_TOPIC,
+    HEDERA_TOPIC,
+    URI,
+    AUTH,
+)
 
-source = "hedera"
-
-if source == "kafka":
-    from kubegrapher.utils.kafka.utils import get_client, subscribe
-else: 
-    from kubegrapher.utils.hedera.utils import get_client, subscribe
+timezone = pytz.timezone(TIMEZONE)
 
 timestamps = []
 
 def processMessage(grapher: Grapher, *args):
-    # print("time: {} received: {}".format(args[0], args[2]))
 
     received = datetime.now(tz=timezone)
-    
     message = json.loads(args[2])
-
     id = message['id']
     send_timestamp = message['timestamp']
     consensus_timestamp = args[0]
@@ -33,10 +35,7 @@ def processMessage(grapher: Grapher, *args):
     write_to_db(grapher, message)
 
     written_to_db = datetime.now(tz=timezone)
-
     timestamps.append((id, send_timestamp, received, written_to_db, consensus_timestamp))
-
-    # print(message)
 
 def write_to_db(grapher: Grapher, message):
     action = message['action']
@@ -83,29 +82,19 @@ def write_to_db(grapher: Grapher, message):
     # grapher.get_counts()
     # grapher.get_subgraph()
 
-
-
 if __name__ == '__main__':
 
-    URI = "bolt://localhost:7687"
-    AUTH = ("neo4j", "password")
     graphdb = Neo4j(URI, AUTH)
     grapher = Grapher(graphdb)
 
-    if source == "kafka":
-
-        conf = {'bootstrap.servers': '10.18.1.35:29092,', 
-            'group.id': 'foo',
+    if DATA_SOURCE == "kafka":
+        conf = {'bootstrap.servers': KAFKA_BROKER_URL, 
+            'group.id': KAFKA_GROUP_ID,
             'auto.offset.reset': 'smallest'}
-
-        topics = ["myTopic"]
-
-        consumer = get_client(conf)
-
-        subscribe(consumer, topics, grapher, processMessage)
-
+        topics = [KAFKA_TOPIC]
+        consumer = Kafka(conf)
+        consumer.subscribe(topics, grapher, processMessage)
     else:
-
-        client = get_client("config.json")
-
-        subscribe(client, "0.0.1003", lambda *args: processMessage(grapher, *args))
+        conf = "config.json"
+        client = Hedera(conf)
+        client.subscribe(HEDERA_TOPIC, lambda *args: processMessage(grapher, *args))
