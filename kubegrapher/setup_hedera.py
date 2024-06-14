478e3c5d6d97463515328ad24940a8833a94f65c
Tarek Zaarour <tareqzaarour@gmail.com>
2024-06-14 12:14:38 +0100

    no need for default_config.json

--- kubegrapher/setup_hedera.py
+++ kubegrapher/setup_hedera.py
@@ -16,7 +16,9 @@ def write_config(filename, config):
 
 if __name__ == "__main__":
 
-    config = {
+    config_file = "config.json"
+
+    config_dict = {
         "network": {
             NETWORK_ACCOUNT_ID: HEDERA_NODE_URL
         },
@@ -27,12 +29,12 @@ if __name__ == "__main__":
         "mirrorNetwork": [HEDERA_MIRROR_NODE_URL]
     }
 
-    write_config('default_config.json', config)
+    write_config(config_file, config_dict)
 
-    client = Hedera("default_config.json")
+    client = Hedera(config_file)
     accountId, privateKey = client.create_account()
 
-    config["operator"]["accountId"] = accountId
-    config["operator"]["privateKey"] = privateKey
+    config_dict["operator"]["accountId"] = accountId
+    config_dict["operator"]["privateKey"] = privateKey
 
-    write_config('config.json', config)
+    write_config(config_file, config_dict)
