e36638d15df3b1aea584651e87aa6399bd49f9f1
Jin Yijie <jinyijie@outlook.com>
2024-08-21 12:13:52 +0100

    fix bug

--- kubegrapher/relations.py
+++ kubegrapher/relations.py
@@ -4,7 +4,7 @@ class Relations(Enum):
     INSTANTIATES_IMAGE, CONFIGMAP_REF, HAS_SELECTOR, \
     HAS_LABEL, HAS_ANNOTATION, MANAGED_BY, \
     SCHEDULED_ON, RUNS_CONTAINER, BELONGS_TO, \
-    HAS_TAINT, STORES_IMAGE, EXPOSES = range(11)
+    HAS_TAINT, STORES_IMAGE, EXPOSES = range(12)
 
     def __get__(self, instance, owner):
             return self.name
